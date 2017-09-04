cf87f26bbad233d7592821de40fe5310c4f71037
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-05 00:01:01 +0200

    Add GUI

---
+++ src/NewJFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class NewJFrame extends JFrame{
+    private JButton insertCarButton;
+    private JButton printListButton;
+    private JButton searchBrandButton;
+    private JButton printBrandButton;
+    private JButton printNumberOfCarsButton;
+    private JButton deleteAllButton;
+    private JTextArea TextArea;
+    private JPanel panel;
+
+   // setContentPane(panel);
+}
---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+    public static void main(String[] args){
+
+        NewJFrame obj = new NewJFrame();
+
+        obj.setVisible(true);
+
+        obj.setResizable(false);
+
+        obj.setSize(800,500);
+    }
+}
