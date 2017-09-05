c0031c8740a8a58c0609e075e06742f1fa012825
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-05 02:38:01 +0200

    Add InsertCar & SearchBrand GUIs

---
+++ src/InsertCar.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class InsertCar {
+    private JPanel insertCarPanel;
+    private JTextField brand;
+    private JTextField model;
+    private JTextField engine;
+    private JTextField power;
+    private JTextField price;
+    private JTextField doors;
+    private JTextField seats;
+    private JTextField color;
+    private JButton addButton;
+    private JButton cancelButton;
+    private JLabel mess;
+}
---
+++ src/SearchBrand.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class SearchBrand {
+    private JTextField brand;
+    private JButton OKButton;
+}
